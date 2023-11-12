e5687522f4a62dee2477d28dc81589c069ebc8b5
Mahmoud Jadaan <masjadaan@gmail.com>
2023-11-12 17:45:36 +0100

    Update syscall_write.s

--- Linux/shellcodes/syscall_write/syscall_write.s
+++ Linux/shellcodes/syscall_write/syscall_write.s
@@ -8,8 +8,7 @@ section .text
 main:
     ; let's write to stdout
     ; ssize_t write(int fildes, const void *buf, size_t nbyte);
-	  ; syscall is passed in rax register
-    mov rax, 1          
+    mov rax, 1          ; syscall is passed in rax register
 	
 	  ; Function parameters are passed in the registers rdi, rsi, and rdx, respectively.
     mov rdi, 1          ; fildes -> 1 for stdout
@@ -19,7 +18,7 @@ main:
 
     ; syscall to exit
     ; void exit(int status);
-    mov rax, 60         ; system call number for exit
+    mov rax, 60         ; 60 = 0x3C system call number for exit
 	
 	  ; One parameter passed in the register rdi
     xor rdi, rdi        ; exit code 0
